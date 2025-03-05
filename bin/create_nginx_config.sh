8bbb8dd7f377e1bfba9f1dfb84e1259b857dc457
Alex Louden <alex@louden.com>
2025-03-05 16:13:11 +0800

    debug

--- bin/create_nginx_config.sh
+++ bin/create_nginx_config.sh
@@ -1,27 +1,25 @@
 #!/bin/bash
 # Debug, echo every command
-#set -x
+set -x
 
 # Use the BUILD_DIR environment variable.
 cd "$BUILD_DIR"
 
 # Sanity checks.
-if [ ! -f "static.json" ]; then
+if [ ! -f "$BUILD_DIR/static.json" ]; then
   echo "Error: static.json not found in ${BUILD_DIR}. Please include static.json and try again."
-  exit 1
-fi
-
-if [ ! -f "package.json" ]; then
-  echo "Error: package.json not found in ${BUILD_DIR}. Cannot install dependencies."
   exit 1
 fi
 
 set -e
 
-echo "Installing EJS dependency..."
-npm install ejs
+cd "$BP_DIR/bin/"
 
+echo "Installing EJS dependency..."
+npm install -g ejs
+
+cd "$BUILD_DIR"
 echo "Running generate-config.js to generate nginx-static.conf.erb..."
-/app/.heroku/node/bin/node generate_config.js "$BUILD_DIR/static.json"
+node "$BP_DIR/bin/generate_config.js" "$BUILD_DIR/static.json"
 
 echo "nginx-static.conf.erb generated successfully."
